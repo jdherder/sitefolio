93016446fa5635d77fc20dcefbe3d7b847650755
Josh Herder <josh.herder@intouchsol.com>
2018-10-24 13:14:17 -0500

    Setting up project for use as an NPM module.

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 /* Node Dependencies */
 const puppeteer = require('puppeteer');
 
-/* Config Dependencies */
-const scenario = require('./config/scenario');
-
 /* Internal Module Dependencies */
 const Util = require('./modules/Util');
 const AemHandler = require('./modules/AemHandler');
@@ -16,108 +13,74 @@ const screenshotFormatExt = 'png';
 
 /* Main */
 
-puppeteer.launch({ ignoreHTTPSErrors: true }).then(async browser => {
-  const page = await browser.newPage();
+exports.run = (scenario) => {
 
-  await page.goto(scenario.primary_url, {
-    waitUntil: 'networkidle0'
-  });
-
-  const aemHandler = new AemHandler(page);
-  await aemHandler.localAuthorLoginCheck();
-
-  Util.createDirectory(screenshotRootPath);
-
-  /* Crawl Intouch Accelerator main nav and grab appropriate links */
-  const navLinks = await page.evaluate((scenario) => {
-    const nav = document.querySelector(scenario.main_nav_selector);
-    const links = Array.from(nav.querySelectorAll(scenario.main_nav_link_selector));
-    const additionalUrls = scenario.additional_urls;
-    const urls = links
-      .filter((link) => {
-        /* Filter out nav links with a 'pointer-events: none' style applied */
-        const style = window.getComputedStyle(link);
-        const filter = Boolean(style.pointerEvents !== 'none');
-        return filter;
-      })
-      .map(link => link.href);
-
-    if (Array.isArray(additionalUrls)) {
-      urls.push.apply(urls, additionalUrls);
-    }
-
-    return urls;
-  }, scenario);
-
-  const processedLinks = navLinks
-    // .slice(0, 3) // FIXME - For faster testing.
-    // .filter(href => href.includes('/plaque-psoriasis/efficacy/dramatic-skin-clearance.html')) // FIXME - For specific testing.
-    .map(href => AemHandler.setWcmModeOnUrl(href));
-
-  console.log(processedLinks);
-
-  const pdfFilePath = Util.generateFilePath(
-    screenshotRootPath,
-    [scenario.label, scenario.screen_width, new Date()],
-    'pdf'
-  );
-  const pdfHandler = new PdfHandler();
-  pdfHandler.open(pdfFilePath);
-
-  for (const href of processedLinks) {
-
-    await page.setViewport({
-      width: scenario.screen_width,
-      height: scenario.screen_height,
-    });
-    
-    await page.goto(href, {
-      waitUntil: 'networkidle0'
-    });
-
-    await aemHandler.localAuthorLoginCheck();
-
-    const title = await page.title();
-
-    console.log(`Processing: ${title}`);
-
-    const imgFilePath = Util.generateFilePath(
-      screenshotRootPath,
-      [scenario.label, title, new Date()],
-      screenshotFormatExt
-    );
-    const screenshotHandler = new ScreenshotHandler(page);
-    await screenshotHandler.takeFullPageScreenshot(imgFilePath);
-
-    /* Add screenshot to full site PDF */
-    pdfHandler.addScreenshotPage(imgFilePath, {
-      pageTitle: title,
-      pageUrl: href
-    });
-
-    /* Process Sub Scenarios */
-    // TODO: Utilize the sub scenario cases to action on them and take additional screenshots
-    // const pageSubScenarios = await page.evaluate((scenario) => {
-    //   const targetClassPrefix = 'screenshot-selector-target';
-    //   const subScenarioTargets = Array.from(document.querySelectorAll('.nav-tabs .dynamic-tab'));
-    //   const subScenarioTargetClasses = [];
-
-    //   // Add a custom unique selector to each DOM node that we will need to interact with for an additional screenshot.
-    //   subScenarioTargets.forEach((node, i) => {
-    //     const className = `${targetClassPrefix}-${i}`;
-    //     node.classList.add(className);
-    //     subScenarioTargetClasses.push(className);
-    //   });
-
-    //   return subScenarioTargetClasses;
-    // }, scenario);
-
+  if (!scenario) {
+    throw new Error('No scenario configuration provided!');
   }
 
-  pdfHandler.close();
+  puppeteer.launch({ ignoreHTTPSErrors: true }).then(async browser => {
+    const page = await browser.newPage();
+  
+    const aemHandler = new AemHandler(page);
+  
+    Util.mkDirByPathSync(screenshotRootPath);
 
-  await ScreenshotHandler.deleteImageFiles(screenshotRootPath, screenshotFormatExt);
+    let scenarioPages = scenario.pages || [];
+  
+    const pdfFilePath = Util.generateFilePath(
+      screenshotRootPath,
+      [scenario.label, scenario.screen_width, new Date()],
+      'pdf'
+    );
+    const pdfHandler = new PdfHandler();
+    pdfHandler.open(pdfFilePath);
+  
+    for (const scenarioPage of scenarioPages) {
 
-  await browser.close();
-});
+      let url = typeof scenarioPage === 'string' ? scenarioPage : scenarioPage.url;
+  
+      if (scenario.aemAuthor) {
+        url = AemHandler.setWcmModeOnUrl(url);
+      }
 
+      await page.setViewport({
+        width: scenario.screen_width,
+        height: scenario.screen_height,
+      });
+      
+      console.log('Navigating to: ', url);
+      await page.goto(url, {
+        waitUntil: 'networkidle0'
+      });
+  
+      if (scenario.aemAuthor) {
+        await aemHandler.localAuthorLoginCheck();
+      }
+  
+      const title = await page.title();
+  
+      console.log(`Processing: ${title}`);
+  
+      const imgFilePath = Util.generateFilePath(
+        screenshotRootPath,
+        [scenario.label, title, new Date()],
+        screenshotFormatExt
+      );
+      const screenshotHandler = new ScreenshotHandler(page);
+      await screenshotHandler.takeFullPageScreenshot(imgFilePath);
+  
+      /* Add screenshot to full site PDF */
+      pdfHandler.addScreenshotPage(imgFilePath, {
+        pageTitle: title,
+        pageUrl: url
+      });
+    }
+  
+    pdfHandler.close();
+  
+    await ScreenshotHandler.deleteImageFiles(screenshotRootPath, screenshotFormatExt);
+  
+    await browser.close();
+  });
+};
--- modules/Util/index.js
+++ modules/Util/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 
 class Util {
   constructor() { }
@@ -15,10 +16,38 @@ class Util {
     return s;
   }
 
-  static createDirectory(path) {
-    if (!fs.existsSync(path)){
-      fs.mkdirSync(path);
+  // Thanks to Mouneer on SO for the below method.
+  static mkDirByPathSync(targetDir, { isRelativeToScript = false } = {}) {
+    if (fs.existsSync(targetDir)){
+      return;
     }
+
+    const sep = path.sep;
+    const initDir = path.isAbsolute(targetDir) ? sep : '';
+    const baseDir = isRelativeToScript ? __dirname : '.';
+  
+    return targetDir.split(sep).reduce((parentDir, childDir) => {
+      const curDir = path.resolve(baseDir, parentDir, childDir);
+      try {
+        fs.mkdirSync(curDir);
+      } catch (err) {
+        if (err.code === 'EEXIST') { // curDir already exists!
+          return curDir;
+        }
+  
+        // To avoid `EISDIR` error on Mac and `EACCES`-->`ENOENT` and `EPERM` on Windows.
+        if (err.code === 'ENOENT') { // Throw the original parentDir error on curDir `ENOENT` failure.
+          throw new Error(`EACCES: permission denied, mkdir '${parentDir}'`);
+        }
+  
+        const caughtErr = ['EACCES', 'EPERM', 'EISDIR'].indexOf(err.code) > -1;
+        if (!caughtErr || caughtErr && curDir === path.resolve(targetDir)) {
+          throw err; // Throw if it's just the last created dir.
+        }
+      }
+  
+      return curDir;
+    }, initDir);
   }
 }
 
